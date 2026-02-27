600618e49b2c07ecf0ea1ea5d2fb83cbaf0b397d
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-02-27 23:22:30 +0200

    Created pages directory with separate page classes for easier navigation

---
+++ pages/LoginModal.js
@@ -0,0 +1,18 @@
+import { BasePage } from './BasePage';
+
+export class LoginPage extends BasePage {
+    constructor(page) {
+        super(page);
+        this.loginTrigger = page.getByText("Login");
+        this.emailInput = page.locator("#email");
+        this.passwordInput = page.locator("#password");
+        this.loginBtn = page.getByRole("button", { name: "Login", exact: true });
+    }
+
+    async login(identifier, password) {
+        await this.loginTrigger.click();
+        await this.emailInput.fill(identifier);
+        await this.passwordInput.fill(password);
+        await this.loginBtn.click();
+    }
+}
---
+++ pages/RegistrationModal.js
@@ -0,0 +1,36 @@
+import { BasePage } from './BasePage';
+
+export class RegistrationModal extends BasePage {
+    constructor(page) {
+        super(page);
+        this.signUpLink = page.getByRole("link", { name: "Sign up" });
+        this.usernameInput = page.locator("#username");
+        this.emailInput = page.locator("#email");
+        this.passwordInput = page.locator("#password");
+        this.getChipsBtn = page.getByRole("button", { name: "Get my free chips" });
+        this.termsContainer = page.locator("[class*='RegistrationModal_termsWrap']");
+        this.termsCheckbox = page.getByText("By submitting my registration");
+        this.continueBtn = page.getByRole("button", { name: "Continue" });
+        this.welcomeBonusText = page.getByText(/CHIPS\+ \d+/);
+        this.startBtn = page.getByRole("button", { name: "Let's start" });
+    }
+
+    async register(user, email, pass) {
+        await this.signUpLink.click();
+        await this.usernameInput.fill(user);
+        await this.emailInput.fill(email);
+        await this.passwordInput.fill(pass);
+        await this.getChipsBtn.click();
+
+        // T&C logic
+        await this.termsCheckbox.scrollIntoViewIfNeeded()
+        await this.termsCheckbox.click();
+        await this.continueBtn.click();
+    }
+
+    async getWelcomeBonusAmount() {
+        const text = await this.welcomeBonusText.textContent();
+        // Regex to extract the number regardless of percentage
+        return Number(text.split(" ")[0].split(",").join(""));
+    }
+}
