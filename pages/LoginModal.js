1d7373fad65ac1164d3eb3fbb48a2121f9939671
Ivan Zhelev <ivxn.zhelev@gmail.com>
2026-02-27 23:32:49 +0200

    Renamed LoginPage and RegisterPage to Modals

--- pages/LoginModal.js
+++ pages/LoginModal.js
@@ -1,6 +1,6 @@
 import { BasePage } from './BasePage';
 
-export class LoginPage extends BasePage {
+export class LoginModal extends BasePage {
     constructor(page) {
         super(page);
         this.loginTrigger = page.getByText("Login");
