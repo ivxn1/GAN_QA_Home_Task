6123666b661d0849e6188a3a2bec4e0ee19a0222
Ivan Zhelev <ivanjelev2004@gmail.com>
2026-02-28 12:44:01 +0200

    Added expect timeout and baseURL for robustness

--- playwright.config.js
+++ playwright.config.js
@@ -22,16 +22,20 @@ export default defineConfig({
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
+    expect: {
+      timeout: 10 * 1000,
+    },
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
         // baseURL: 'http://localhost:3000',
-
+        baseURL: 'https://jackentertainment.lv-stg.gameaccount.com/',
         screenshot: "only-on-failure",
         video: "retain-on-failure",
         trace: "retain-on-failure",
+        viewport: { width: 1920, height: 1080 }
     },
 
     /* Configure projects for major browsers */
